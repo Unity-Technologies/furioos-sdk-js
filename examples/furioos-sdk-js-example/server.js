92ac688f90e8e1d98e317db39b3c06dd7d00d518
Ben Salili-James <benhjames@sky.com>
2021-09-24 14:20:35 +0100

    Migrate to TypeScript

--- examples/furioos-sdk-js-example/server.js
+++ examples/furioos-sdk-js-example/server.js
@@ -1,16 +1,16 @@
-const express=require('express');
-const path = require('path');
-const WebSocket = require('ws');
-const app=express();
-app.use(express.static(__dirname + '/'));
+const express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
+const app = express();
+app.use(express.static(__dirname + "/"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
 
-const port =8080;
-app.listen(port,()=>{
-console.log(`App running on ${port}`);
+const port = 8080;
+app.listen(port, () => {
+  console.log(`App running on ${port}`);
 });
 
 const wss = new WebSocket.Server({
@@ -20,10 +20,10 @@ const wss = new WebSocket.Server({
       // See zlib defaults.
       chunkSize: 1024,
       memLevel: 7,
-      level: 3
+      level: 3,
     },
     zlibInflateOptions: {
-      chunkSize: 10 * 1024
+      chunkSize: 10 * 1024,
     },
     // Other options settable:
     clientNoContextTakeover: true, // Defaults to negotiated value.
@@ -31,18 +31,16 @@ const wss = new WebSocket.Server({
     serverMaxWindowBits: 10, // Defaults to negotiated value.
     // Below options specified as default values.
     concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
+    threshold: 1024, // Size (in bytes) below which messages
     // should not be compressed.
-  }
+  },
 });
 
-wss.on('connection', function connection(ws) {
-  console.log("Server WS connected")
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message); 
+wss.on("connection", function connection(ws) {
+  console.log("Server WS connected");
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
 
-    
-    
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
