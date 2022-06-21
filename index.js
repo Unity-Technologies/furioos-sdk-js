913533b89a41fc14e257485ccc1710fb9187b1e8
PaulGovaere <paul.govaere@unity3d.com>
2022-06-21 16:58:54 +0200

    fix: global export

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var Player = require("./classes/Player.js");
+const { Player, FS_QUALITY_VALUES, FS_SDK_EVENTS_NAME, QUALITY_VALUES } = require("./classes/Player.js");
 
 module.exports = {
-  Player
+  Player,
+  FS_SDK_EVENTS_NAME,
+  FS_QUALITY_VALUES,
+  QUALITY_VALUES,
 }
